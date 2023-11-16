ba73f20ba76e6f6aefd117f8879b838aab041102
oAnthonyG <anthonytguilherme@hotmail.com>
2023-11-16 19:09:27 -0300

    firts commit

---
+++ primeiro_desafio.rb
@@ -0,0 +1,9 @@
+puts "Digite seu nome: "
+nome = gets.chomp.to_s
+puts "Digite seu sobrenome: "
+sobrenome = gets.chomp.to_s
+puts "Digite sua idade: "
+idade = gets.chomp.to_s
+
+
+puts "Olá #{nome} #{sobrenome} vc tem #{idade} anos"
