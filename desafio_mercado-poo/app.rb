43a287196fd2a2d190b0c4eebf4a8178715d69b8
oAnthonyG <anthonytguilherme@hotmail.com>
2023-11-29 14:22:04 -0300

    att

--- desafio_mercado-poo/app.rb
+++ desafio_mercado-poo/app.rb
@@ -2,11 +2,11 @@ require_relative 'product'
 require_relative 'marketplace'
 
 produto = Product.new
-produto.name = 'Forma para bolo redonda'
-produto.price = 34.99
+produto.name = 'Celular Motorola moto G(20)'
+produto.price = 899
 produto1 = Product.new
-produto1.name = 'Bolo de cenoura com calda de chocolate'
-produto1.price = 39.99
+produto1.name = 'Placa de video RTX 4090'
+produto1.price = 12.499
 
 Market.new(produto.name, produto.price).purchase
 Market.new(produto1.name, produto1.price).purchase
