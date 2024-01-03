4015b24380f44a1c3f9c019e82778d3c6dcc5039
oAnthonyG <anthonytguilherme@hotmail.com>
2024-01-03 19:16:16 -0300

    namespace and proc and lambda

---
+++ namespcace.rb
@@ -0,0 +1,17 @@
+#serve como um container para agrupar objetos relacionados
+
+module PalavraReversa
+  def self.puts text
+    print text.reverse.to_s
+  end
+end
+
+module PalavraNormal
+  def self.puts text
+    print text
+  end
+end
+
+
+PalavraReversa::puts 'O resultado é'
+PalavraNormal::puts 'O resultado é'
--- teste.rb
+++
@@ -1,9 +0,0 @@
-def paperwork(n, m)
-  n < 0 || m < 0 ? 0 : n * m
-end
-
-
-# 'n' é quantidade de pessoas
-# 'm' é a quantidade de paginas
-#calcular as paginas em branco é n vezes m ?
-# se 'n' ou 'm' for 0 retorna 0
